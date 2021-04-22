89eca71a5e1bf9230578dcd79e0f136a9161cc5a
Molly Wilson <molly_wilson1@outlook.com>
2021-04-21 23:47:37 -0500

    Save and load plans from local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,29 +3,55 @@ const displayDate = function () {
     $("#currentDay").text(moment().format("dddd, MMM Do"));
 };
 
-const displayTime = function () {
-    
+const displayPlanner = function () {
+
     const startTime = 9;
     const endTime = 12 + 5;
-    
+
+    const savedPlans = loadPlans();
+
     for (let hour = startTime; hour <= endTime; hour++) {
-        
+
         createTimeBlockEl(hour);
+        $("#" + hour).find("textarea").val(savedPlans[hour]);
     }
 };
 
 const createTimeBlockEl = function (hour) {
     const template = $($("#hour-block-template").html());
-    
-    //in case I need to referance stuff like saving to local storage
-    // template.find(".time-block").attr("id", hour);
 
-    template.find(".hour").text(moment({hour}).format("hA"));
+    template.attr("id", hour);
+
+    template.find(".hour").text(moment({ hour }).format("hA"));
     $("#workday").append(template);
 }
 
+const savePlans = function (plans) {
+    localStorage.setItem("plans", JSON.stringify(plans))
+};
+
+const loadPlans = function () {
+    let savedPlans = localStorage.getItem("plans");
+    if (!savedPlans) {
+        return {};
+    }
+
+    savedPlans = JSON.parse(savedPlans);
+    return savedPlans;
+};
 
 displayDate();
-displayTime();
+displayPlanner();
 
-//.saveBtn on click don't need specific id
+$(".saveBtn").on("click", function(event) {
+    const timeBlockEl = $(event.target).parents(".time-block");
+    console.log(timeBlockEl);
+    const hour = timeBlockEl.attr("id");
+    const text = timeBlockEl.find("textarea").val();
+
+    const savedPlans = loadPlans();
+
+    savedPlans[hour] = text;
+    savePlans(savedPlans);
+});
+
