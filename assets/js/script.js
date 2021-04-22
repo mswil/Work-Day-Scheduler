0d72ad0d3ba747dc744f1ad1bace159e14f19e12
Molly Wilson <molly_wilson1@outlook.com>
2021-04-21 22:14:19 -0500

    function to create a time-block element, using the template in index.html, for each hour from start time to end time

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,5 +3,29 @@ const displayDate = function () {
     $("#currentDay").text(moment().format("dddd, MMM Do"));
 };
 
-displayDate();
+const displayTime = function () {
+    
+    const startTime = 9;
+    const endTime = 12 + 5;
+    
+    for (let hour = startTime; hour <= endTime; hour++) {
+        
+        createTimeBlockEl(hour);
+    }
+};
 
+const createTimeBlockEl = function (hour) {
+    const template = $($("#hour-block-template").html());
+    
+    //in case I need to referance stuff like saving to local storage
+    // template.find(".time-block").attr("id", hour);
+
+    template.find(".hour").text(moment({hour}).format("hA"));
+    $("#workday").append(template);
+}
+
+
+displayDate();
+displayTime();
+
+//.saveBtn on click don't need specific id
